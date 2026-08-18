e49ebde1c3a2ca091aca8bf2c40e04fccca0f457
Raiyan K <96327738+Amerax@users.noreply.github.com>
2026-08-18 15:43:39 -0400

    Add loop and showSequence functionality
    Implement loop and showSequence functions to display LED sequence.

--- main.ino
+++ main.ino
@@ -31,3 +31,45 @@ void setup() {
 
   showSequence();
 }
+
+void loop(){
+  
+}
+
+void showSequence() {
+  delay(500);
+  for (int i = 0; i < sequenceLength; i++) {
+
+    playMove(sequence[i]);
+
+    delay(250);
+  }
+}
+
+void playMove(int move) {
+
+  if (move == 0) {
+
+    // red  led
+    digitalWrite(RED_LED, HIGH);
+    tone(BUZZER, 500);
+
+    delay(250);
+
+    digitalWrite(RED_LED, LOW);
+    noTone(BUZZER);
+
+  } else {
+
+    // blue led
+    digitalWrite(BLUE_LED, HIGH);
+    tone(BUZZER, 900);
+
+    delay(250);
+
+    digitalWrite(BLUE_LED, LOW);
+    noTone(BUZZER);
+  }
+
+  delay(100);
+}
